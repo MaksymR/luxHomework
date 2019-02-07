39b78d80a762b11a938b91265a2827ca1afd0764
Maksym Riaboshapka <makmr11@gmail.com>
2019-02-07 10:20:30 +0200

    2nd commit added homework01 and MyMain.java

---
+++ src/com/example/homework01/gitBranches_sortedAlgorithms/MyMain.java
@@ -0,0 +1,9 @@
+package com.example.homework01.gitBranches_sortedAlgorithms;
+
+public class MyMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+
+}
